73b48677a922d7671828003bf438e4c64816fdb1
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-06-14 12:50:37 +0700

    Added route switch animation

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -18,6 +18,25 @@ watch(
 <template>
   <div class="flex flex-grow flex-col overflow-auto">
     <Header></Header>
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <Transition name="route" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </router-view>
   </div>
 </template>
+
+<style scoped>
+.route-enter-from {
+  transform: translateY(-100%);
+  position: absolute;
+}
+.route-leave-to {
+  transform: translate(100%, 100%) rotateZ(90deg);
+  position: absolute;
+}
+.route-enter-active,
+.route-leave-active {
+  transition: transform 0.3s cubic-bezier(0.8, 0, 0.29, 0.99);
+}
+</style>
