d67e6a7aa35fdf501eb5541f276f740264f60415
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-06-18 18:54:16 +0700

    PWA --test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg, jpeg, png, ttf}"],
+      },
+      manifest: {
+        name: "Dashboard",
+        short_name: "MyApp",
+        description: "Dashboard",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
