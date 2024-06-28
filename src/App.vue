a2748b773a756c5a4bd8e0b4c1edbdb63eee4293
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-06-28 14:26:47 +0700

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 import Navigation from "./components/Navigation/Navigation.vue";
 import Content from "./components/Content.vue";
 import { useConfigStore, useAppStore } from "./store";
-import { getImage } from "./utils/styles";
+
 import { computed, onMounted, ref } from "vue";
 import { getFreeGames } from "./api";
 import Message from "./components/Message.vue";
+import Background from "./components/Background.vue";
 
 const store = useConfigStore();
 const appStore = useAppStore();
@@ -23,18 +24,12 @@ onMounted(async () => {
 
   appStore.updateFreeGames(resp.data);
 });
-console.log(getImage(store.Background.images))
 </script>
 
 <template>
   <Message v-if="message" :message="message" :onClick="() => (message = '')" />
-  <img
-    :src="getImage(store.Background.images)"
-    alt="Background"
-    referrerpolicy="no-referrer"
-    class="fixed inset-0 z-[-1] w-dvw h-dvh object-cover"
-  />
-  <div class="bg-center bg-no-repeat">
+  <Background />
+  <div class="bg-center bg-no-repeat" referrer-policy="no-referrer">
     <div
       class="flex h-dvh gap-5 py-2.5 pl-2.5 pr-5"
       :style="`backdrop-filter: blur(${store.Background.blur || 0}px)`"
@@ -54,9 +49,3 @@ console.log(getImage(store.Background.images))
   background-color: v-bind(getScrollBarActiveColor);
 }
 </style>
-
-<!-- 
-:style="`
-      background-color:${store.Background.color || '#000'}; 
-      background-image: url(${getImage(store.Background.images) || 'none'});
-      background-size: ${store.Background.size};`" -->
---
+++ src/components/Background.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useConfigStore } from "../store";
+import { getImage } from "../utils/styles";
+
+const store = useConfigStore();
+</script>
+
+<template>
+  <img
+    :src="getImage(store.Background.images)"
+    alt="Background"
+    referrerpolicy="no-referrer"
+    class="fixed inset-0 z-[-1] h-dvh w-dvw object-cover"
+  />
+</template>
