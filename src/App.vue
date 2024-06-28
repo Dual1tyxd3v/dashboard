f4718f6756ee8b369c263c19008da7a986608b96
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-06-28 14:34:08 +0700

    Refactoring background

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import Navigation from "./components/Navigation/Navigation.vue";
 import Content from "./components/Content.vue";
 import { useConfigStore, useAppStore } from "./store";
-
 import { computed, onMounted, ref } from "vue";
 import { getFreeGames } from "./api";
 import Message from "./components/Message.vue";
@@ -29,7 +28,10 @@ onMounted(async () => {
 <template>
   <Message v-if="message" :message="message" :onClick="() => (message = '')" />
   <Background />
-  <div class="bg-center bg-no-repeat" referrer-policy="no-referrer">
+  <div
+    class="bg-center bg-no-repeat"
+    referrer-policy="no-referrer"
+  >
     <div
       class="flex h-dvh gap-5 py-2.5 pl-2.5 pr-5"
       :style="`backdrop-filter: blur(${store.Background.blur || 0}px)`"
--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useConfigStore } from "../store";
 import { getImage } from "../utils/styles";
 
 const store = useConfigStore();
+
+const isLoading = ref(true);
 </script>
 
 <template>
-  <img
-    :src="getImage(store.Background.images)"
-    alt="Background"
-    referrerpolicy="no-referrer"
-    class="fixed inset-0 z-[-1] h-dvh w-dvw object-cover"
-  />
+  <div
+    class="fixed inset-0 z-[-1] h-dvh w-dvw"
+    :style="`
+      background-color:${store.Background.color || '#000'}`"
+  >
+    <img
+      :src="getImage(store.Background.images)"
+      alt="Background"
+      referrerpolicy="no-referrer"
+      :class="`h-full w-full object-cover transition-opacity duration-300 ${isLoading ? 'opacity-0' : 'opacity-100'}`"
+      @load="isLoading = false"
+    />
+  </div>
 </template>
